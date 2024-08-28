adb73bdf203a4a3c09b79041739d3afa5d6469d0
Reece Hutchison <hutchisora@icloud.com>
2024-08-28 08:56:14 -0600

    1807A

---
+++ 1807A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i=0; i<n; ++i) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if(a+b==c)
+            cout << "+" << endl;
+        else
+            cout << "-" << endl;
+    }
+}
